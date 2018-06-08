ec21fb6a4d8dc9ed24e001e3f0d9e7f2a7ba38f6
irfan87 <nerve2009@yahoo.com>
2018-06-08 23:04:32 +0800

    refactoring the code

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="textHeader">Title</h1>
+        <h1 style={{fontSize: 54, ...defaultStyle}}>Title</h1>
         <Aggregate />
         <Aggregate />
         <Filter />
